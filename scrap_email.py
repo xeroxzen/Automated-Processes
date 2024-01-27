f17a7f26db30af2086f8879beee850f839007858
Google Jr <andilembele020@gmail.com>
2024-01-27 11:40:45 +0200

    Iteration #1

--- scrap_email.py
+++ scrap_email.py
@@ -1,10 +1,23 @@
 import imaplib
 import email
 from email.header import decode_header
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
 
-# Connect to the IMAP server
-mail = imaplib.IMAP4_SSL('your_email_server.com')
-mail.login('your_email@example.com', 'your_password')
+# Set your Gmail credentials
+email_address = 'andilembele020@gmail.com'
+credentials_file = 'path/to/credentials.json'  # You need to create a credentials file using Google Cloud Console
+
+# Set up the Gmail API scope
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+# Authenticate using OAuth
+flow = InstalledAppFlow.from_client_secrets_file(credentials_file, SCOPES)
+credentials = flow.run_local_server(port=0)
+
+# Connect to the Gmail IMAP server
+mail = imaplib.IMAP4_SSL('imap.gmail.com')
+mail.login(email_address, credentials.token)
 
 # Select the mailbox you want to work with (e.g., 'inbox')
 mail.select('inbox')
